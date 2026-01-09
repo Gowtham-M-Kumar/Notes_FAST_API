c2878ba2f462bb53a602231fa3ea75b1e8e20cf2
Gowtham M Kumar <gmk@Gowthams-MacBook-Air.local>
2026-01-09 16:46:35 +0530

    Allow Vercel frontend origin in CORS

--- app/core/config.py
+++ app/core/config.py
@@ -34,7 +34,8 @@ class Settings(BaseSettings):
         "http://localhost:3000", 
         "http://localhost:3001",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001"
+        "http://127.0.0.1:3001",
+        "https://note-frontend-livid.vercel.app"
     ]
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
